9bcf54c652f9e0c5dc984ed70303f7bbcb61fcdc
m1yag1 <mike.arbelaez@rice.edu>
2019-04-18 00:10:36 -0500

    upgrade jinja2 and make config more clear

--- {{cookiecutter.project_name}}/instance/config.py
+++ {{cookiecutter.project_name}}/instance/config.py
@@ -8,7 +8,13 @@ FLASK_ENV = os.environ.get('FLASK_ENV', 'development')
 FLASK_APP = 'wsgi.py'
 DEBUG = os.environ.get('FLASK_DEBUG', False)
 SECRET_KEY = os.environ.get('SESSION_SECRET', str(uuid.uuid4()))
-SQLALCHEMY_DATABASE_URI = make_database_url()
+
+# SQLALCHEMY
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', make_database_url())
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# HEROKU
+# Heroku requires the SERVER_NAME to be set to the app name ex. myapp.herokuapp.com
 SERVER_NAME = os.environ.get('FLASK_SERVER_NAME', None)
+# Heroku needs this set to 'https' to set all connections to https
 PREFERRED_URL_SCHEME = os.environ.get('FLASK_PREFERRED_URL_SCHEME', 'http')
